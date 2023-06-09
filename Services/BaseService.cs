c6296502f7e1a91bb4cdc60b0853919e96434a1c
ihsan Ç <a.ihsan.celik96@gmail.com>
2023-06-09 14:36:16 +0300

    Create BaseService.cs

---
+++ Services/BaseService.cs
@@ -0,0 +1,15 @@
+﻿namespace rtperson.Services
+{
+    public abstract class BaseService
+    {
+        private readonly ILogService _logService;
+
+        public BaseService(ILogService logService)
+        {
+            _logService = logService;
+        }
+
+        public void WriteLog(string message) => _logService.LogAsync(message);
+        public void WriteException(Exception ex) => _logService.LogAsync(ex);
+    }
+}
