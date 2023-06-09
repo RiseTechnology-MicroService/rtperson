6d90d6fdc969d66dcc54b27f9e30cabff0dc20b3
ihsan Ç <a.ihsan.celik96@gmail.com>
2023-06-09 14:35:32 +0300

    Created Async Logging Service

---
+++ Services/ILogService.cs
@@ -0,0 +1,8 @@
+﻿namespace rtperson.Services
+{
+    public interface ILogService
+    {
+        Task LogAsync(string message);
+        Task LogAsync(Exception ex);
+    }
+}
---
+++ Services/LogService.cs
@@ -0,0 +1,41 @@
+﻿namespace rtperson.Services
+{
+    public class LogService : ILogService
+    {
+        private string _version = string.Empty;
+        private string _application = string.Empty;
+
+        private const string ErrorLogType = "error";
+        private const string InfoLogType = "info";
+
+        private readonly LogRequestService _logRequestService;
+
+        public LogService(IConfiguration configuration, LogRequestService logRequestService)
+        {
+            var version = configuration.GetSection("Application:Version").Value;
+            if (version == null)
+                throw new Exception("Application version is not defined in appsettings.json");
+
+            var application = configuration.GetSection("Application:Name").Value;
+            if (application == null)
+                throw new Exception("Application name is not defined in appsettings.json");
+
+            _version = version;
+            _application = application;
+            _logRequestService = logRequestService;
+        }
+        public async Task LogAsync(string message)
+        {
+            await _logRequestService
+                .SendLogAsync(new SendLogModel(_application, _version, InfoLogType, message, string.Empty))
+                .ConfigureAwait(false);
+        }
+
+        public async Task LogAsync(Exception ex)
+        {
+            await _logRequestService
+               .SendLogAsync(new SendLogModel(_application, _version, ErrorLogType, ex.Message, ex.StackTrace))
+               .ConfigureAwait(false);
+        }
+    }
+}
